ea1b5c96f7e179392ee1701ca99ae4ac3979e6b5
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2020-10-29 19:33:52 -0400

    correct native function class

--- global_functions.py
+++ global_functions.py
@@ -4,11 +4,11 @@ from loxcallable import LoxCallable
 
 
 class Clock(LoxCallable):
-    def call():
+    def call(self):
         return time.time()
 
-    def arity():
+    def arity(self):
         return 0
 
-    def __str__():
+    def __str__(self):
         return "<native_fn:Clock>"
