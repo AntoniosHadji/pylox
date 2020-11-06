cb3a420cbd63024bc1d75573d728a2f4700c8b17
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2020-11-06 06:39:24 -0500

    ch12.2

--- loxclass.py
+++ loxclass.py
@@ -1,6 +1,26 @@
-class LoxClass:
+from typing import Any, List
+
+from loxcallable import LoxCallable
+
+
+class LoxClass(LoxCallable):
     def __init__(self, name: str):
         self.name = name
 
     def __repr__(self):
         return self.name
+
+    def call(self, interpreter, arguments: List[Any]) -> Any:
+        instance: LoxInstance = LoxInstance(self)
+        return instance
+
+    def arity(self) -> int:
+        return 0
+
+
+class LoxInstance:
+    def __init__(self, klass: LoxClass):
+        self.klass = klass
+
+    def __repr__(self):
+        return self.klass.name + " instance"
