534ef5fed30ee50bbb13d6ac5fe034ad1b558be3
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2020-11-07 08:55:11 -0500

    add dataclass behavior test

--- tests/dataclass_test.py
+++ tests/dataclass_test.py
@@ -22,37 +22,46 @@ class E:
     c: int
 
 
-# normal
-n = N(1, 2, 3)
-print(n)
-d = {n: "normal_class"}
-print(d)
+def test_normal_class():
+    # normal
+    n1 = N(1, 2, 3)
+    n2 = N(1, 2, 3)
+    d = {n1: "normal_class 1", n2: "normal_class 2"}
+    assert len(d) == 2
 
 
-# frozen
-d1 = D(1, 2, 3)
-print(d1)
-print(type(d1))
-print(type(n))
-d2 = D(1, 2, 3)
-print(d2)
-print("id(d2):", id(d2))
-print("id(d1):", id(d1))
-print(d1 == d2)
-print(d1 == n)
-print(hash(n))
-print(hash(d1))
-print(hash(d2))
-
-# frozen + eq
+def test_frozen_dataclass():
+    # frozen
+    d1 = D(1, 2, 3)
+    d2 = D(1, 2, 3)
+    n = N(1, 2, 3)
+    assert id(d1) != id(d2)
+    assert d1 == d2
+    assert d1 != n
+    assert d2 != n
+    assert hash(d1) == hash(d2)
+    assert hash(d1) != hash(n)
+    assert hash(d2) != hash(n)
 
 
-e = E(1, 2, 3)
-f = E(1, 2, 3)
-print(e, f)
-print(e == f)
-print(hash(e), hash(f))
-d.update({d1: "dataclass1", d2: "dataclass2"})
-print(d)
-d.update({e: "dataclass1", f: "dataclass2"})
-print(d)
+def test_frozen_eq_dataclass():
+    # frozen + eq
+    e = E(1, 2, 3)
+    f = E(1, 2, 3)
+    assert e != f
+    assert id(e) != id(f)
+    assert hash(e) != hash(f)
+
+
+def test_dict():
+    d1 = D(1, 2, 3)
+    d2 = D(1, 2, 3)
+    e = E(1, 2, 3)
+    f = E(1, 2, 3)
+    n = N(1, 2, 3)
+    d = {n: "normal_class"}
+    assert len(d) == 1
+    d.update({d1: "dataclass1", d2: "dataclass2"})
+    assert len(d) == 2
+    d.update({e: "dataclass1", f: "dataclass2"})
+    assert len(d) == 4
